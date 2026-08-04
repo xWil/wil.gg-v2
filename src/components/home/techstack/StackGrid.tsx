e53e9606fd5972eb780c91c86a7370675ca98ffa
xWil <xwildev@gmail.com>
2026-08-04 21:17:01 +0100

    Fixed issues with grid not resizing properly

--- src/components/home/techstack/StackGrid.tsx
+++ src/components/home/techstack/StackGrid.tsx
@@ -2,7 +2,7 @@ import StackList from "./StackList.tsx";
 
 function StackGrid() {
     return (
-        <div className="grid grid-cols-4 gap-12">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-12">
             <StackList title="Languages" items={["Java", "JS/TS", "HTML & CSS"]}/>
             <StackList title="Frameworks" items={["Spring", "React"]}/>
             <StackList title="Database" items={["MySQL", "PostgreSQL"]}/>
