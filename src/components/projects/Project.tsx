a2dd93d7ba6bfc2c88a7b623f3c4f3bb6d443443
xWil <xwildev@gmail.com>
2026-08-10 21:04:48 +0100

    Added projects sticking to screen as you scroll

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -4,15 +4,17 @@ import type {DetailProps} from "./DetailList.tsx";
 import ProjectLink, {type ProjectLinkProps} from "./ProjectLink.tsx";
 
 interface ProjectProps {
+    visible: boolean,
+    stick: boolean,
     header: HeaderProps,
     description: string,
     details: DetailProps[]
     link: ProjectLinkProps
 }
 
-function Project({ header, description, details, link }: ProjectProps) {
+function Project({ visible, stick, header, description, details, link }: ProjectProps) {
     return (
-        <div className="min-h-screen w-full pb-16 pt-32 px-8 md:px-8 lg:px-16 xl:px-24 flex flex-col items-center justify-center">
+        <div className={`min-h-screen w-full pb-16 pt-32 px-8 md:px-8 lg:px-16 xl:px-24 flex flex-col items-center justify-center ${stick ? "sticky top-0" : ""} ${visible ? "visible" : "invisible"}`}>
             <div className="px-4 md:px-12 lg:px-20 xl:px-28 w-full h-fit flex flex-col">
                 <span className="text-6xl text-text-primary font-bold mb-8">Projects</span>
                 <ProjectHeader header={header}/>
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,9 +1,24 @@
 import Project from "./Project.tsx";
+import {useRef, useState} from "react";
+import {observe} from "../../ObserveVisibleHook.tsx";
 
 function Projects() {
+    const [firstVisible, setFirstVisible] = useState(false);
+    const firstRef = useRef<HTMLDivElement>(null);
+    observe(firstRef, () => setFirstVisible(true), () => setFirstVisible(false), {threshold: 1})
+
+    const [secondVisible, setSecondVisible] = useState(false);
+    const secondRef = useRef<HTMLDivElement>(null);
+    observe(secondRef, () => setSecondVisible(true), () => setSecondVisible(false), {threshold: 1})
+
+    //const [thirdVisible, setThirdVisible] = useState(false);
+    //const thirdRef = useRef<HTMLDivElement>(null);
+    //observe(thirdRef, () => setThirdVisible(true), () => setThirdVisible(false), {threshold: 1})
+
     return (
-        <section id="projects" className="h-fit w-full">
-            <Project header={{ icon: "/pfp.gif", alt: "Project icon", name: "Imposter" }}
+        <section id="projects" className="relative h-fit w-full">
+            <Project visible={true} stick={firstVisible}
+                     header={{ icon: "/pfp.gif", alt: "Project icon", name: "Imposter" }}
                      description="Imposter is a web-based party game where one player is designated as the imposter, and everyone has to guess who!"
                      details={[
                          {category: "Language", details: "Java, JavaScript, HTML & CSS"},
@@ -11,21 +26,26 @@ function Projects() {
                          {category: "Backend", details: "Spring / SpringBoot"}
                      ]} link={{ link: "https://imposter.wil.gg/", text: "imposter.wil.gg" }}
             />
-            <Project header={{ icon: "/pfp.gif", alt: "Project icon", name: "VLRScraper" }}
+            <Project visible={!firstVisible} stick={secondVisible}
+                     header={{ icon: "/pfp.gif", alt: "Project icon", name: "VLRScraper" }}
                      description="VLRScraper is a web-scraper and statistics analysis tool for VCT match data, sourced from VLR."
                      details={[
                          {category: "Language", details: "Java, JavaFX, CSS"},
                          {category: "Database", details: "MySQL, SQLite"}
                      ]} link={{ link: "https://https://github.com/xWil", text: "github" }}
             />
-            <Project header={{ icon: "/pfp.gif", alt: "Project icon", name: "SubwayCrafters" }}
-                     description="VLRScraper is a web-scraper and statistics analysis tool for VCT match data, sourced from VLR."
+            <Project visible={!secondVisible} stick={true}
+                     header={{ icon: "/pfp.gif", alt: "Project icon", name: "SubwayCrafters" }}
+                     description="SubwayCrafters is a feature-rich, infinitly generating runner game, built on Spigot."
                      details={[
                          {category: "Language", details: "Java"},
                          {category: "Platform", details: "Spigot, Paper"},
                          {category: "Database", details: "PostgreSQL, MySQL, SQLite"}
                      ]} link={{ link: "https://https://github.com/xWil", text: "github" }}
             />
+            <div className="h-screen w-full invisible"/>
+            <div ref={firstRef} className="absolute top-[25%] size-2"/>
+            <div ref={secondRef} className="absolute top-[50%] size-2"/>
         </section>
     )
 }
